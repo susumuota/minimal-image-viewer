ec77c1c5b0e8b338f622a193d1c2fb1a3b8302cc
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2022-12-20 18:31:45 +0900

    add contextBridge.exposeInMainWorld.

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,23 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('api', {
+  dialog: (options: Electron.OpenDialogOptions): Promise<string[] | undefined> => ipcRenderer.invoke('dialog', options),
+  glob: (pattern: string): Promise<string[]> => ipcRenderer.invoke('glob', pattern),
+  storeSet: (key: string, value: unknown): Promise<void> => ipcRenderer.invoke('store-set', key, value),
+  storeGet: (key: string): Promise<unknown> => ipcRenderer.invoke('store-get', key),
+  devtools: (): Promise<void> => ipcRenderer.invoke('devtools'),
+  quit: (): Promise<void> => ipcRenderer.invoke('quit'),
+})
+
+declare global {
+  interface Window {
+    api: {
+      dialog: (options: Electron.OpenDialogOptions) => Promise<string[] | undefined>,
+      glob: (pattern: string) => Promise<string[]>,
+      storeSet: (key: string, value: unknown) => Promise<void>,
+      storeGet: (key: string) => Promise<unknown>,
+      devtools: () => Promise<void>,
+      quit: () => Promise<void>,
+    }
+  }
+}
